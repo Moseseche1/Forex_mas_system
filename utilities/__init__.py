95ba19079734926e58cfb6fee6ae666518f6d7d8
Moseseche1 <onyekemoseseche@gmail.com>
2025-09-06 12:03:36 +0100

    Update __init__.py

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -1,17 +1,4 @@
-from fastapi import APIRouter, Request
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
-import os
+# Utilities package initialization
+from .error_handler import error_handler, retry_with_backoff, CircuitOpenError
 
-router = APIRouter()
-
-# Setup templates
-templates = Jinja2Templates(directory=os.path.join(os.path.dirname(__file__), "templates"))
-
-@router.get("/dashboard", response_class=HTMLResponse)
-async def dashboard(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
-@router.get("/", response_class=HTMLResponse)
-async def root_dashboard(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+__all__ = ['error_handler', 'retry_with_backoff', 'CircuitOpenError']
